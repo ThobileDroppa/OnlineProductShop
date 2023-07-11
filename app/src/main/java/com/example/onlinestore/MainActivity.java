1122af3a0cb92cf7d16a0bb60319c3b5864eb7bd
ThobileDroppa <Thobilej@droppa.co.za>
2023-07-11 17:08:48 +0200

    tryout commit

--- app/src/main/java/com/example/onlinestore/MainActivity.java
+++ app/src/main/java/com/example/onlinestore/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent = new Intent(MainActivity.this, WelcomeActvity.class);
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
                 startActivity(intent);
             }
         },3000);
